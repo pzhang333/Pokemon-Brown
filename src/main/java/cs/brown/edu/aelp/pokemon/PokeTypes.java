061375b9ecf5c41e4e428e912bf95035cbaba22f
Patrick Zhang <patrick_zhang1@brown.edu>
2018-03-31 18:56:43 -0400

    Pokemon class added with builder pattern

---
+++ src/main/java/cs/brown/edu/aelp/pokemon/PokeTypes.java
@@ -0,0 +1,12 @@
+package cs.brown.edu.aelp.pokemon;
+
+/**
+ * Pokemon and Pokemon move types, according to Generation 4. References this
+ * online chart:
+ * https://bulbapedia.bulbagarden.net/wiki/Type/Type_chart#Generations_II-V
+ */
+public enum PokeTypes {
+  NORMAL, FIGHT, FLYING, POISON, GROUND, ROCK,
+  BUG, GHOST, STEEL, FIRE, WATER, GRASS, ELECTRIC,
+  PSYCHIC, ICE, DRAGON, DARK;
+}
