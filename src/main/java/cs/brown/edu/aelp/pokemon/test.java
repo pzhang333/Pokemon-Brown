625b3bb1195ff5416e4e152a3bc0d0b9dd3bee6d
Patrick Zhang <patrick_zhang1@brown.edu>
2018-04-01 18:57:31 -0400

    Changed Move class to use builder patttern to avoid constructor with too many parameters

---
+++ src/main/java/cs/brown/edu/aelp/pokemon/test.java
@@ -0,0 +1,12 @@
+package cs.brown.edu.aelp.pokemon;
+
+public class test {
+
+  public static void main(String[] args) {
+    final int x = 3;
+    x = x - 1;
+    System.out.println(x);
+
+  }
+
+}
