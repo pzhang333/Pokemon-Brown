556074c45b5f06c3eccc17b642e4ca58b56b77ab
abrevnov17 <tbdapplications@gmail.com>
2018-04-19 23:59:44 -0400

    Restructuring, Client Side Additions, and Setup for Demo/Testing
    Restructured what information we are sending in our messages. Added a client-side component. Made it runnable for testing/demo purposes (can run by executing ./run --gui and then navigating to http://localhost:4567/main). Still some errors and not fully fleshed out in a couple places (especially on the client side), but is very adaptable for future changes.

--- src/main/java/cs/brown/edu/aelp/networking/GamePacket.java
+++ src/main/java/cs/brown/edu/aelp/networking/GamePacket.java
@@ -9,56 +9,25 @@ import java.util.stream.Collectors;
 
 public class GamePacket {
   
-  private Coordinate3d position;
   private Collection<Player> otherPlayers;
-  private Map<Coordinate3d, Integer> background;
-  private Integer userState;
+  private Player player;
 
-  public GamePacket(Coordinate3d playerPosition, 
-      Collection<Player> otherPlayers, 
-      int userState, 
-      Map<Coordinate3d, Integer> backgroundMap) {
+  public GamePacket(Player player, 
+      Collection<Player> otherPlayers, Map<Coordinate3d, Integer> backgroundMap) {
     
-    this.position = playerPosition;
-    this.background = backgroundMap;
-    this.setOtherPlayers(localize(otherPlayers));
-    this.userState = userState;
-    
+    this.setOtherPlayers(localize(player.getPosition(), otherPlayers));
+    this.setPlayer(player);
   }
   
   // Helper functions:
   
-  private Collection<Player> localize(Collection<Player> players) {
+  private Collection<Player> localize(Coordinate3d playerPosition, Collection<Player> players) {
     return players.stream().filter(p -> p.getPosition().getZ() 
-        == this.position.getZ())
+        == playerPosition.getZ())
         .collect(Collectors.toList());
   }
 
   // Getters and Setters for Package Properties:
-  
-  public Coordinate3d getPosition() {
-    return position;
-  }
-
-  public void setPosition(Coordinate3d position) {
-    this.position = position;
-  }
-
-  public Map<Coordinate3d, Integer> getBackground() {
-    return background;
-  }
-
-  public void setBackground(Map<Coordinate3d, Integer> background) {
-    this.background = background;
-  }
-
-  public Integer getUserState() {
-    return userState;
-  }
-
-  public void setUserState(Integer userState) {
-    this.userState = userState;
-  }
 
   public Collection<Player> getOtherPlayers() {
     return otherPlayers;
@@ -66,6 +35,14 @@ public class GamePacket {
 
   public void setOtherPlayers(Collection<Player> otherPlayers) {
     this.otherPlayers = otherPlayers;
+  }
+
+  public Player getPlayer() {
+    return player;
+  }
+
+  public void setPlayer(Player player) {
+    this.player = player;
   }
 
 }
--- src/main/java/cs/brown/edu/aelp/pokemon/Main.java
+++ src/main/java/cs/brown/edu/aelp/pokemon/Main.java
@@ -1,26 +1,32 @@
 package cs.brown.edu.aelp.pokemon;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import org.eclipse.jetty.websocket.api.Session;
+import java.io.File;
 
-import cs.brown.edu.aelp.Player.Player;
-import cs.brown.edu.aelp.general_datastructures.Coordinate3d;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+
 import cs.brown.edu.aelp.networking.PlayerWebSocketHandler;
+import freemarker.template.Configuration;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import spark.ExceptionHandler;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 import spark.Spark;
+import spark.TemplateViewRoute;
+import spark.template.freemarker.FreeMarkerEngine;
 
 /**
  * The Main class entry point.
  */
 public final class Main {
-  
-  // we use this to map sessions to session ids
-  public static Map<Session, String> sessionToID = new ConcurrentHashMap<>();
-  // We use this for websockets to continue creating unique session id's
-  public static int sessionNumber = 0;
-  
-  // maps session id's to Player objects
-  public static Map<String, Player> sessionIDToPlayer;
+
+  private static final int DEFAULT_PORT = 4567;
 
   /**
    * @param args
@@ -34,7 +40,6 @@ public final class Main {
    * Private constructor for main.
    */
   private Main() {
-
   }
 
   /**
@@ -44,8 +49,91 @@ public final class Main {
    *          Command line arguments
    */
   private void run(String[] args) {
-    Spark.webSocket("/play", PlayerWebSocketHandler.class);
+
+    // Parse command line arguments
+    OptionParser parser = new OptionParser();
+    parser.accepts("gui");
+    parser.accepts("port").withRequiredArg().ofType(Integer.class)
+        .defaultsTo(DEFAULT_PORT);
+    OptionSet options = parser.parse(args);
+
+    if (options.has("gui")) {
+      runSparkServer((int) options.valueOf("port"));
+    }
+
     System.out.println("Hello, World!");
+
+    // temporary repl
+    int count = 0;
+    while (5 != 6) {
+      if (count >= 100000) {
+        count = 0;
+      }
+      if (count % 10000 == 0) {
+        // this should be in our game loop and execute every tick
+        PlayerWebSocketHandler.sendGamePackets();
+      }
+    }
+  }
+
+  private void runSparkServer(int port) {
+    Spark.webSocket("/game", PlayerWebSocketHandler.class);
+    
+    Spark.port(port);
+    Spark.externalStaticFileLocation("src/main/resources/static");
+    Spark.exception(Exception.class, new ExceptionPrinter());
+    
+    FreeMarkerEngine freeMarker = createEngine();
+
+    Spark.get("/main", new FrontHandler(), freeMarker);
+
+    // Setup Spark Routes
+  }
+  
+  /**
+   * Handle requests to the front page of our website.
+   */
+  private static class FrontHandler implements TemplateViewRoute {
+    @Override
+    public ModelAndView handle(Request req, Response res) {
+      Map<String, Object> variables = ImmutableMap.of("title",
+          "Pokemon", "content", "something", "readmessage", "something", "message", "something");
+      return new ModelAndView(variables, "query.ftl");
+    }
+  }
+
+  // Spark set up:
+
+  private static FreeMarkerEngine createEngine() {
+    Configuration config = new Configuration();
+    File templates = new File("src/main/resources/spark/template/freemarker");
+    try {
+      config.setDirectoryForTemplateLoading(templates);
+    } catch (IOException ioe) {
+      System.out.printf("ERROR: Unable use %s for template loading.%n",
+          templates);
+      System.exit(1);
+    }
+    return new FreeMarkerEngine(config);
+  }
+
+  // Spark error handling:
+
+  /**
+   * Display an error page when an exception occurs in the server.
+   */
+  private static class ExceptionPrinter implements ExceptionHandler {
+    @Override
+    public void handle(Exception e, Request req, Response res) {
+      res.status(500);
+      StringWriter stacktrace = new StringWriter();
+      try (PrintWriter pw = new PrintWriter(stacktrace)) {
+        pw.println("<pre>");
+        e.printStackTrace(pw);
+        pw.println("</pre>");
+      }
+      res.body(stacktrace.toString());
+    }
   }
 
 }
