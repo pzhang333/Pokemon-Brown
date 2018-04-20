ef2bfb526f6aaace42c53b9c0d4c03dc6d7c2987
Louis Kilfoyle <louis_kilfoyle@brown.edu>
2018-04-20 22:59:08 +0100

    Merged player/user class, added a Spark endpoint for registration

--- src/main/java/cs/brown/edu/aelp/networking/PlayerWebSocketHandler.java
+++ src/main/java/cs/brown/edu/aelp/networking/PlayerWebSocketHandler.java
@@ -1,76 +1,72 @@
 package cs.brown.edu.aelp.networking;
 
-import java.util.Map;
-
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
-import org.eclipse.jetty.websocket.api.*;
-import org.eclipse.jetty.websocket.api.annotations.*;
-
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-
-import cs.brown.edu.aelp.Player.Player;
+import cs.brown.edu.aelp.pokemmo.data.authentication.User;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 
 @WebSocket
 public class PlayerWebSocketHandler {
 
-    private static final Gson GSON = new Gson();
-    private static final Queue<Session> sessions = new ConcurrentLinkedQueue<>();
-    private static int nextId = 0;
-    
-    // maps session id's to Player objects
-    private static Map<Session, Player> sessionToPlayer;
-    
-    private static enum MESSAGE_TYPE {
-      CONNECT,
-      GAME_PACKET,
-      UPDATE_USER,
-      CLIENT_UPDATE
-    }
-  
-    @OnWebSocketConnect
-    public void onConnect(Session session) throws Exception {
-      
-      sessions.add(session);
-      
-      // Building the CONNECT message
-      JsonObject main = new JsonObject();
-      main.addProperty("type", 0);
-      JsonObject message = new JsonObject();
-      message.addProperty("id", nextId);
-      main.add("payload", message);
-      
-      nextId++;
+  private static final Gson GSON = new Gson();
+  private static final Queue<Session> sessions = new ConcurrentLinkedQueue<>();
+  private static int nextId = 0;
 
-      // sending connect message
-      session.getRemote().sendString(GSON.toJson(main));
-    }
+  // maps session id's to Player objects
+  private static Map<Session, User> sessionToPlayer;
 
-    @OnWebSocketClose
-    public void onClose(Session session, int statusCode, String reason) {
-      // Here we perform any actions we wish to do on a user closing 
-      // connection (i.e. saving position to database, etc...)
-      sessions.remove(session);
-    }
+  private static enum MESSAGE_TYPE {
+    CONNECT, GAME_PACKET, UPDATE_USER, CLIENT_UPDATE
+  }
 
-    @OnWebSocketMessage
-    public void onMessage(Session session, String message) {
-      // we have received a websocket message
-      // this message will be a JSON object (in string form)
-      JsonObject received = GSON.fromJson(message, JsonObject.class);
-      if (received.get("type").getAsInt() == MESSAGE_TYPE.GAME_PACKET.ordinal()) {
-        // a user has received an updated game packet
-      } 
-      else if (received.get("type").getAsInt() == MESSAGE_TYPE.UPDATE_USER.ordinal()) {
-        
-      }
+  @OnWebSocketConnect
+  public void onConnect(Session session) throws Exception {
+
+    sessions.add(session);
+
+    // Building the CONNECT message
+    JsonObject main = new JsonObject();
+    main.addProperty("type", 0);
+    JsonObject message = new JsonObject();
+    message.addProperty("id", nextId);
+    main.add("payload", message);
+
+    nextId++;
+
+    // sending connect message
+    session.getRemote().sendString(GSON.toJson(main));
+  }
+
+  @OnWebSocketClose
+  public void onClose(Session session, int statusCode, String reason) {
+    // Here we perform any actions we wish to do on a user closing
+    // connection (i.e. saving position to database, etc...)
+    sessions.remove(session);
+  }
+
+  @OnWebSocketMessage
+  public void onMessage(Session session, String message) {
+    // we have received a websocket message
+    // this message will be a JSON object (in string form)
+    JsonObject received = GSON.fromJson(message, JsonObject.class);
+    if (received.get("type").getAsInt() == MESSAGE_TYPE.GAME_PACKET.ordinal()) {
+      // a user has received an updated game packet
+    } else if (received.get("type").getAsInt() == MESSAGE_TYPE.UPDATE_USER
+        .ordinal()) {
+
     }
-    
-    // sends the game packets to all open sessions
-    public static void sendGamePackets() {
-      PacketSender.sendGamePackets(sessions, sessionToPlayer);
-    }
+  }
+
+  // sends the game packets to all open sessions
+  public static void sendGamePackets() {
+    PacketSender.sendGamePackets(sessions, sessionToPlayer);
+  }
 
 }
---
+++ src/main/java/cs/brown/edu/aelp/util/JsonFile.java
@@ -0,0 +1,29 @@
+package cs.brown.edu.aelp.util;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JsonFile {
+
+  private static final Gson GSON = new Gson();
+  private Map<String, String> vals = new HashMap<>();
+
+  public JsonFile(String path) throws IOException {
+    try (FileReader fr = new FileReader(path);
+        JsonReader reader = new JsonReader(fr)) {
+      this.vals = GSON.fromJson(reader, this.vals.getClass());
+    }
+  }
+
+  public String getKey(String key) {
+    if (vals.containsKey(key)) {
+      return vals.get(key);
+    }
+    return null;
+  }
+
+}
