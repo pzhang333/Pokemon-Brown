e62770092069e7147171908760f555da2b1a108e
Ethan Sattler <ethan_sattler@brown.edu>
2018-04-20 20:14:54 -0400

    Fixed regex, remove sha1 hashing :(

--- src/main/java/cs/brown/edu/aelp/pokemmo/server/RegisterHandler.java
+++ src/main/java/cs/brown/edu/aelp/pokemmo/server/RegisterHandler.java
@@ -1,11 +1,14 @@
 package cs.brown.edu.aelp.pokemmo.server;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 import com.google.gson.Gson;
+
 import cs.brown.edu.aelp.pokemmo.data.DataSource;
 import cs.brown.edu.aelp.pokemmo.data.DataSource.AuthException;
 import cs.brown.edu.aelp.pokemmo.data.authentication.User;
-import java.util.HashMap;
-import java.util.Map;
 import spark.QueryParamsMap;
 import spark.Request;
 import spark.Response;
@@ -40,21 +43,22 @@ public class RegisterHandler implements Route {
     return GSON.toJson(vars);
   }
 
-  public boolean validateInput(String name, String pass, String email)
-      throws AuthException {
+  private static Pattern emailNamePatterj = Pattern.compile(
+      "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+
+  public boolean validateInput(String name, String pass, String email) throws AuthException {
 
     if (name.length() <= 3 || name.length() > 20) {
-      throw new AuthException(
-          "Username must have greater than 3 and fewer than 21 characters.");
+      throw new AuthException("Username must have greater than 3 and fewer than 21 characters.");
     }
 
-    if (!pass.matches(
-        "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}")) {
+    System.out.println(pass);
+    if (!pass.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}")) {
       throw new AuthException("Password must be at least 8 characters,"
           + " containing an upper case letter, a lower case letter, a number, and a symbol.");
     }
 
-    if (!email.matches("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\\\.[A-Z]{2,6}$")) {
+    if (!email.toUpperCase().matches("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$")) {
       throw new AuthException("Email must be of a valid format.");
     }
 
