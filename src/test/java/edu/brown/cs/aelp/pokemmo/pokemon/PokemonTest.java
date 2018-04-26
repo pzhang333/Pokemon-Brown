10ab2510bee2a5d369920d89957ef06e020b7214
Louis Kilfoyle <louis_kilfoyle@brown.edu>
2018-04-26 19:08:52 +0100

    Resolved test build issue

--- src/test/java/edu/brown/cs/aelp/pokemmo/pokemon/PokemonTest.java
+++ src/test/java/edu/brown/cs/aelp/pokemmo/pokemon/PokemonTest.java
@@ -2,20 +2,16 @@ package edu.brown.cs.aelp.pokemmo.pokemon;
 
 import static org.junit.Assert.assertNotNull;
 
-import org.junit.Test;
-
 import cs.brown.edu.aelp.pokemmo.pokemon.Pokemon;
+import org.junit.Test;
 
 public class PokemonTest {
   @Test
   public void basicConstructionTests() {
     Pokemon a = new Pokemon.Builder(1).build();
     assertNotNull(a);
-    Pokemon b = new Pokemon.Builder(2)
-        .withGender(1)
-        .withExp(1000)
-        .withMaxHp(250)
-        .build();
+    Pokemon b = new Pokemon.Builder(2).withGender(1).withExp(1000)
+        .withBaseHp(250).build();
     assertNotNull(b);
   }
 }
