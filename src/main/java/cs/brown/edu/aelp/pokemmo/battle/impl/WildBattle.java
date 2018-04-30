711eef9677ce7e9d30be6dccfe6beffe59d8b804
Louis Kilfoyle <louis_kilfoyle@brown.edu>
2018-04-30 21:03:10 +0100

    Updated wild battle packet

--- src/main/java/cs/brown/edu/aelp/pokemmo/battle/impl/WildBattle.java
+++ src/main/java/cs/brown/edu/aelp/pokemmo/battle/impl/WildBattle.java
@@ -2,24 +2,26 @@ package cs.brown.edu.aelp.pokemmo.battle.impl;
 
 import cs.brown.edu.aelp.pokemmo.battle.Arena;
 import cs.brown.edu.aelp.pokemmo.battle.Battle;
+import cs.brown.edu.aelp.pokemmo.data.authentication.User;
 import cs.brown.edu.aelp.pokemmo.pokemon.Pokemon;
-import cs.brown.edu.aelp.pokemmo.trainer.Trainer;
 
 public class WildBattle extends Battle {
 
   private final Pokemon wild;
+  private final User user;
 
-  public WildBattle(Integer id, Arena arena, Trainer trainer, Pokemon wild) {
+  public WildBattle(Integer id, Arena arena, User user, Pokemon wild) {
     super(id, arena);
     this.wild = wild;
+    this.user = user;
+  }
+
+  public User getUser() {
+    return this.user;
   }
 
   public Pokemon getWildPokemon() {
     return wild;
   }
 
-  @Override
-  public BattleType getBattleType() {
-    return BattleType.WILD_BATTLE;
-  }
 }
