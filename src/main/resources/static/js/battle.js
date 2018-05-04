88db4b5d891108c6aee069e94ec9035e71586bb4
Ethan Sattler <ethan_sattler@brown.edu>
2018-05-03 20:42:08 -0400

    Updated UI

--- src/main/resources/static/js/battle.js
+++ src/main/resources/static/js/battle.js
@@ -491,7 +491,7 @@ Battle.drawPokemon = function(fore, bg) {
 	
 }
 
-Battle.drawMessage = function(text, size) {
+Battle.drawMessage = async function(text, size) {
 	
 	Battle.clearMessageText();
 	
@@ -509,6 +509,8 @@ Battle.drawMessage = function(text, size) {
 		
 		Battle.messageText.size = size;
 		
+		
+		await Battle.messageText;
 		Battle.panel.add(Battle.messageText);
 	} else {
 		Battle.messageText.value = text;
