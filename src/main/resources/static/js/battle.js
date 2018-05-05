0c118f021b59f35503a256f85a08c7a128fea71d
Ethan Sattler <ethan_sattler@brown.edu>
2018-05-05 05:31:30 -0400

    Fixed ui

--- src/main/resources/static/js/battle.js
+++ src/main/resources/static/js/battle.js
@@ -156,7 +156,7 @@ Battle.doSwitch = function(pOut, pIn, cb) {
 	let fore = (pOut.id == Battle.frontPokemon.id) ? pIn : undefined;
 	let bg = (pOut.id == Battle.backPokemon.id) ? pIn : undefined;
 
-	if (pOut.id != Battle.frontPokemon.id && pOut != Battle.backPokemon.id) {
+	if (pOut.id != Battle.frontPokemon.id && pOut.id != Battle.backPokemon.id) {
 		Battle.endBattle();
 	}
 
